b9b45dc8bbed2da00ceaf76e995bdff0d102e46e
Pedro D <pedro.dias.developer@gmail.com>
2021-06-20 21:19:06 -0300

    Fizemos um algoritmo para calcular a sequência de Fibonacci.

---
+++ sequencia_de_fibonacci.py
@@ -0,0 +1,19 @@
+def fibonacci(num):
+    """
+    Fizemos uma função que pega como parâmetro a quantidade de resultados que o usuário deseja.
+    E aplica a sequência de de Fibonacci.
+    """
+    i, a, b = 0, 1, 1
+    sequencia_fibonacci = []
+    while i < num:
+        sequencia_fibonacci.append(a)
+        c = a + b
+        a, b = b, c
+        i += 1
+    return sequencia_fibonacci
+
+
+# Fazendo o "input" para receber a quantidade de resultados da sequência.
+input_num = int(input("Insira a quantidade da sequência de Fibonacci: "))
+# Imprimimos o resultado para o usuário.
+print(f'O resultado da sequência foi: {", ".join(str(num) for num in fibonacci(input_num))}')
